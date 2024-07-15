845cc8f31841850fe0c6a8783e4d064fc945eda6
Samukelisiwe Monalisa Ntusi <148154184+Monalicous@users.noreply.github.com>
2024-07-15 21:54:39 +0400

    Documents
    HTML, CSS, JavaScript,

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.progress-html').css('width', '60%');
+    $('.progress-css').css('width', '50%');
+    $('.progress-javascript').css('width', '30%');
+    $('.progress-vscode').css('width', '60%');
+    $('.progress-github').css('width', '70%');
+  });
+  
