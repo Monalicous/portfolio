163b20daa8acb9af581abdd4385dc103f873e9ab
Samukelisiwe Monalisa Ntusi <148154184+Monalicous@users.noreply.github.com>
2024-11-02 18:38:44 +0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-    $('.progress-html').css('width', '60%');
+    $('.progress-html').css('width', '70%');
     $('.progress-css').css('width', '50%');
     $('.progress-javascript').css('width', '30%');
     $('.progress-vscode').css('width', '60%');
-    $('.progress-github').css('width', '70%');
+    $('.progress-github').css('width', '50%');
+    $('.progress-virtual').css('width', '75%');
+
   });
   
